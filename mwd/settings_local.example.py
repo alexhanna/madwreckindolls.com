fada21fc2f4e3510b6c0edfb4c856a068ae4c11b
Dan Silvers <dan@silvers.net>
2012-12-04 10:17:02 -0600

    Example of local settings file

---
+++ mwd/settings_local.example.py
@@ -0,0 +1,33 @@
+CRISPY_TEMPLATE_PACK = "bootstrap"
+
+TEMPLATE_DIRS = (
+    "/Users/dan/projects/mwd/templates/"
+)
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/Users/dan/mwd.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = '/Users/dan/projects/mwd/media/static/'
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    '/Users/dan/projects/mwd/static/',
+)
