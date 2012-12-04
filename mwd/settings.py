0a0837877847fc0e29b26130a231a3931794587f
Dan Silvers <dan@silvers.net>
2012-12-03 18:52:07 -0600

    Activated admin site, added legal headaches app

--- mwd/settings.py
+++ mwd/settings.py
@@ -125,11 +125,12 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'crispy_forms',
     
+    'legal_headache',
     'accounts',
     'registration',
 )
