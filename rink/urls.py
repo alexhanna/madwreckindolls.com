201e87daffea0367f21557c3ba0895ea45e5ea33
Dan Silvers <dan@silvers.net>
2013-02-15 19:37:07 -0600

    Initial rink portal, login/logout working

---
+++ rink/urls.py
@@ -0,0 +1,41 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
+urlpatterns = patterns('',
+                       (r'^login/$', 
+                        'django.contrib.auth.views.login', 
+                        {'template_name': 'rink/login.html'}),
+
+                       (r'^logout/$', 
+                        'django.contrib.auth.views.logout', 
+                        {'template_name': 'rink/logout.html'}),
+
+                       (r'^password-change/$', 
+                        'django.contrib.auth.views.password_change', 
+                        {'template_name': 'rink/password_change_form.html'}),
+
+                       (r'^password-change/done/$', 
+                        'django.contrib.auth.views.password_change_done', 
+                        {'template_name': 'rink/password_change_done.html'}),
+
+                       (r'^password-reset/$', 
+                        'django.contrib.auth.views.password_reset', 
+                        {'template_name': 'rink/password_reset_form.html',
+                         'email_template_name': 'emails/password_reset_email.html'}),
+
+                       (r'^password-reset/done/$', 
+                        'django.contrib.auth.views.password_reset_done', 
+                        {'template_name': 'rink/password_reset_done.html'}),
+
+                       (r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 
+                        'django.contrib.auth.views.password_reset_confirm', 
+                        {'template_name': 'rink/password_reset_confirm.html'}),
+
+                       (r'^reset/done/$', 
+                        'django.contrib.auth.views.password_reset_complete', 
+                        {'template_name': 'rink/password_reset_complete.html'}),
+
+                       (r'^$',
+                        'rink.views.index'),
+
+)
