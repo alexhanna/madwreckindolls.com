2b2e508f7966cfbf549d34e8b82d5ed92ec78782
Dan Silvers <dan@silvers.net>
2013-02-17 02:38:16 -0600

    Password generator utility function

--- mwd/utilities.py
+++ mwd/utilities.py
@@ -18,3 +18,55 @@ import string
 def random_string(length):
     pool = string.letters + string.digits
     return ''.join(random.choice(pool) for i in xrange(length))
+
+
+
+
+
+
+
+
+# http://www.ironzebra.com/news/30/create-random-pronounceable-passwords-in-pythondjango
+#
+# adaption of:
+# http://stackoverflow.com/questions/5501477/any-python-password-generators-that-are-readable-and-pronounceable
+# originally written by Greg Haskins
+
+import itertools
+
+initial_consonants = (set(string.ascii_lowercase) - set('aeiou')
+                      # remove those easily confused with others
+                      - set('qxc')
+                      # add some crunchy clusters
+                      | set(['bl', 'br', 'cl', 'cr', 'dr', 'fl',
+                             'fr', 'gl', 'gr', 'pl', 'pr', 'sk',
+                             'sl', 'sm', 'sn', 'sp', 'st', 'str',
+                             'sw', 'tr'])
+                      )
+
+final_consonants = (set(string.ascii_lowercase) - set('aeiou')
+                    # confusable
+                    - set('qxcsj')
+                    # crunchy clusters
+                    | set(['ct', 'ft', 'mp', 'nd', 'ng', 'nk', 'nt',
+                           'pt', 'sk', 'sp', 'ss', 'st'])
+                    )
+
+vowels = 'aeiou' # we'll keep this simple
+
+# each syllable is consonant-vowel-consonant "pronounceable"
+syllables = map(''.join, itertools.product(initial_consonants, 
+                                           vowels, 
+                                           final_consonants))
+
+# you could throw in number combinations, maybe capitalized versions... 
+
+def gibberish(wordcount, wordlist=syllables):
+    return random.sample(wordlist, wordcount)
+    
+def get_pronounceable_password(wordcount=2, digitcount=2):
+    numbermax = 10 ** digitcount
+    password = ''.join(gibberish(wordcount))
+    if digitcount >= 1:
+        password += str(int(random.random()*numbermax))
+    return password
