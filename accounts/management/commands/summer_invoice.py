6dd5c4deda8faf1c39aff0e31ceab131f3bc8822
Dan Silvers <dan@silvers.net>
2013-05-31 19:14:34 -0500

    Summer dues billing scripts and email template.

---
+++ accounts/management/commands/summer_invoice.py
@@ -0,0 +1,82 @@
+from django.core.management.base import BaseCommand, CommandError
+from accounts.models import Skater, SkaterStatus, SkateSessionPaymentSchedule, Invoice, generate_scheduled_invoice
+from mwd import settings
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+import csv
+
+class Command(BaseCommand):
+    args = "<csv file>"
+    help = "Generate an invoice for the selected period and email them."
+
+    def handle(self, *args, **options):
+        
+        month_name = "Summer Session (June)"
+
+        # 6 - June
+        # 7 - July
+        # 8 - Aug
+        billing_period = 6
+
+        count = 0
+        for file in args:
+          with open(file, 'rb') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            for row in reader:
+                count = count + 1
+                if count != 1:
+                    name = row[0]
+                    email = row[1]
+                    dues = row[2]
+
+                    try:
+                        skater = Skater.objects.get(email__exact=email)
+                    except Skater.DoesNotExist:
+                        skater = False
+                        self.stdout.write( str(count) + "\t==== NOT FOUND, NOT INVOICED: " + email)
+
+                    if skater:
+
+                        invoiced = False
+
+                        "Check if we've already generated an invoice for this user"
+                        try:
+                            invoice = Invoice.objects.get(skater=skater, schedule=schedule)
+                            invoiced = True
+                        except Invoice.DoesNotExist:
+                            pass
+
+                        if invoiced:
+                            self.stdout.write( str(count) + "\t~~~~ ALREADY INVOICED - " + email + " / " + skater.get_short_name() )
+                        else:
+                            generate_scheduled_invoice(skater, schedule)
+                            self.stdout.write( str(count) + "\tOK - " + name + "\t" + email + "\t" + str(amount))
+
+                            sk = Skater.objects.get(id=skater.id)
+                            self.stdout.write( "$" + str(skater.balance) + "\t" + sk.get_short_name() )
+
+
+                            html = render_to_string('emails/summer_invoice.html',
+                                {
+                                    'skater' : sk,
+                                    'skater_short_name': sk.get_short_name(),
+                                }
+                            )
+
+                            msg = EmailMultiAlternatives(
+                                    "Mad Wreckin' Dolls " + month_name + " Dues",
+                                    html,
+                                    settings.FROM_EMAIL,
+                                    [ skater.email ],
+                                    [ settings.FROM_EMAIL ],
+                                    headers = {
+                                        'Reply-To' : settings.FROM_EMAIL,
+                                        'CC' : "finance@madwreckindolls.com",
+                                        'Content-Type' : 'text/html'
+                                    },
+                            )
+                
+                            msg.content_subtype = "html"
+                            msg.send(fail_silently = False)
+                
+
