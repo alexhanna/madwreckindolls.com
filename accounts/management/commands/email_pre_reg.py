16c136bf8d7d30e29686ad5a700a95339e6fbb9d
Dan Silvers <dan@silvers.net>
2013-08-31 22:17:22 -0500

    Pre registration email script.

--- accounts/management/commands/email_pre_reg.py
+++ accounts/management/commands/email_pre_reg.py
@@ -11,37 +11,46 @@ class Command(BaseCommand):
     help = "Email pre registration skaters"
 
     def handle(self, *args, **options):
+        count = 0
+        for file in args:
+          with open(file, 'rb') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            for row in reader:
+                    email = row[4]
 
-        count = 1
+                    if email != "":
 
-        skaters = Skater.objects.filter(pk=1)
-        #skaters = Skater.objects.all()
+                        try:
+                            skater = Skater.objects.get(email__exact=email)
+                        except Skater.DoesNotExist:
+                            skater = False
+                            self.stdout.write( str(count) + " !!!!!!!!!!!!!!! OH CRAP ----- Problem with: " + email)
 
-        for skater in skaters:
+                    if skater:
 
-            html = render_to_string('emails/pre-reg-invite.html',
-                    {
-                        'skater' : skater,
-                        'skater_short_name': skater.get_short_name(),
-                    }
-                )
+                        html = render_to_string('emails/pre-reg-invite.html',
+                            {
+                                'skater' : skater,
+                                'skater_short_name': skater.get_short_name(),
+                            }
+                        )
 
-            msg = EmailMultiAlternatives(
-                "Mad Wreckin' Dolls Fall 2013 Registration",
-                html,
-                settings.FROM_EMAIL,
-                [ skater.email ],
-                [ settings.FROM_EMAIL ],
-                headers = {
-                    'Reply-To' : settings.FROM_EMAIL,
-                    'CC' : "finance@madwreckindolls.com",
-                    'Content-Type' : 'text/html'
-                },
-            )
+                        msg = EmailMultiAlternatives(
+                                "Mad Wreckin' Dolls Fall 2013 Registration",
+                                html,
+                                settings.FROM_EMAIL,
+                                [ skater.email ],
+                                [ settings.FROM_EMAIL ],
+                                headers = {
+                                    'Reply-To' : settings.FROM_EMAIL,
+                                    'CC' : "finance@madwreckindolls.com",
+                                    'Content-Type' : 'text/html'
+                                },
+                        )
 
-            msg.content_subtype = "html"
-            msg.send(fail_silently = False)
-            
-	    self.stdout.write( str(count) + " OK! " + skater.email )
-            count = count + 1
+                        msg.content_subtype = "html"
+                        msg.send(fail_silently = False)
+                	    
+                        self.stdout.write( str(count) + " OK! " + skater.email )
+                        count = count + 1
 
